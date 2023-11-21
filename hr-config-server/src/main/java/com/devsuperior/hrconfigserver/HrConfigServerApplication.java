12b1e5a2c2438ea3d244496501d9caf276dc9f0e
Wesley Souza Roman <wesley.sdesouza@gmail.com>
2023-11-21 11:22:18 -0300

    Project hr-user create

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,6 +1,5 @@
 package com.devsuperior.hrconfigserver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,11 +9,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class HrConfigServerApplication implements CommandLineRunner{
 
-	@Value("${spring.cloud.config.server.git.username}")
-	private String username;
+	//@Value("${spring.cloud.config.server.git.username}")
+	//private String username;
 	
-	@Value("${spring.cloud.config.server.git.password}")
-	private String password;
+	//@Value("${spring.cloud.config.server.git.password}")
+	//private String password;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
